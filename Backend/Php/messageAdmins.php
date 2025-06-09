c7bdc03eabd7143fc4c69d4a14c3569904e1aa9d
Johan Moyane <papimoyane2468@gmail.com>
2025-06-09 16:57:55 +0200

    attempting to fix session clashes

--- Backend/Php/messageAdmins.php
+++ Backend/Php/messageAdmins.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Pragma: no-cache");
+header("Expires: 0");
 define("BASE_PATH", dirname(__DIR__, 2));
 require (BASE_PATH . "/Backend/Php/Database.php");
 
